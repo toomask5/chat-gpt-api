bbd4d829c0b7edfd486586433aba193b9328d255
toomask5 <toomas.kants@ruumum.com>
2023-12-25 14:13:08 +0200

    first commit

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
 import { config } from "dotenv"
+
+
 config()
 
-import { Configuration, OpenAIApi } from "openai"
-import readline from "readline"
+const openAi = new OpenAIApi(
+  new Configuration({
+    apiKey: process.env.OPEN_AI_API_KEY,
+  })
 
+console.log(process.env.API_KEY)
+
+import OpenAI from "openai"
+
+const openai = new OpenAI();
+
+
+async function main() {
+    const stream = await openai.chat.completions.create({
+        model: "gpt-4",
+        messages: [{ role: "user", content: "Say this is a test" }],
+        stream: true,
+    });
+    for await (const chunk of stream) {
+        process.stdout.write(chunk.choices[0]?.delta?.content || "");
+    }
+}
+
+main();
+
+
+
+//import readline from "readline"
+
+/*
 const openAi = new OpenAIApi(
   new Configuration({
     apiKey: process.env.OPEN_AI_API_KEY,
@@ -24,3 +53,4 @@ userInterface.on("line", async input => {
   console.log(response.data.choices[0].message.content)
   userInterface.prompt()
 })
+*/
